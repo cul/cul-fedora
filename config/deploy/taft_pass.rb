486b968d2e11b920caa75f30f9ac9ab61313052b
James Stuart <tastyhat@jamesstuart.org>
2010-05-27 16:03:08 -0400

    changed branch to scv-53

--- config/deploy/taft_pass.rb
+++ config/deploy/taft_pass.rb
@@ -1,6 +1,7 @@
 set :domain,      "taft.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
+set :branch, "scv-53"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
