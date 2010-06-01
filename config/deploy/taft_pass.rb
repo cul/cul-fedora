203311f318a2ade1d6d9ad082319dd0ef52fa896
James Stuart <tastyhat@jamesstuart.org>
2010-06-01 12:40:02 -0400

    changed deploy branch to master

--- config/deploy/taft_pass.rb
+++ config/deploy/taft_pass.rb
@@ -1,7 +1,7 @@
 set :domain,      "taft.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/#{application}/"
 set :user, "deployer"
-set :branch, "scv-53"
+set :branch, "master"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
