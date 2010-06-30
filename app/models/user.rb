4079f7841854df832d81d79746f87fb47d424456
James Stuart <tastyhat@jamesstuart.org>
2010-06-30 14:50:56 -0400

    added support for setting personal ldap info via user model, consolidated various migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
+require_dependency 'vendor/plugins/blacklight/app/models/user.rb'
+
 class User < ActiveRecord::Base
-   
   before_validation_on_create :set_personal_info_via_ldap
 
   def set_personal_info_via_ldap
     if wind_login
-      self.active = true
-
-      if (entry = Connectors::CunixLdap.lookup_uid(wind_login).first)
+      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
+      entry = entry.first
+       
+      if entry
         self.email = entry[:mail].to_s
         self.last_name = entry[:sn].to_s
         self.first_name = entry[:givenname].to_s
       end
     end
     
-    return to_s
+    return self
   end
 end
--- db/migrate/20090127164730_create_users.rb
+++ db/migrate/20090127164730_create_users.rb
@@ -1,13 +1,29 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
+      t.string :first_name, :length => 30
+      t.string :last_name, :length => 40
+      t.boolean :admin
       t.string :login, :unique=>true, :null=>false
+      t.string :wind_login, :unique => true
       t.string :email, :unique=>true
       t.string :crypted_password
+      t.string :persistence_token
+      t.integer :login_count, :default => 0, :null => false
       t.text :last_search_url
-      t.datetime :last_login
+      t.datetime :last_login_at
+      t.datetime :last_request_at
+      t.datetime :current_login_at
+      t.string :last_login_ip
+      t.string :current_login_ip
       t.timestamps
     end
+
+    add_index :users, :login
+    add_index :users, :wind_login
+    add_index :users, :persistence_token
+    add_index :users, :last_request_at
+
   end
   
   def self.down
