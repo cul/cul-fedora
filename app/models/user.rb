5dd62679ccca51f247263a62092f7c3297d03426
James Stuart <tastyhat@jamesstuart.org>
2010-07-01 12:39:24 -0400

    reindented user file, just to test functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ActiveRecord::Base
     if wind_login
       entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
       entry = entry.first
-       
+
       if entry
         self.email = entry[:mail].to_s
         self.last_name = entry[:sn].to_s
         self.first_name = entry[:givenname].to_s
       end
     end
-    
+
     return self
   end
 end
