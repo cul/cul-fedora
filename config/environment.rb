4974e05f306e031094f9d36b09056ce5e97f37c0
James Stuart <tastyhat@jamesstuart.org>
2010-07-07 14:57:45 -0400

    changed admin homepage, linked to jira project

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-RELEASE_STAMP = '0.1.0'
+RELEASE_STAMP = '0.3.0'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
