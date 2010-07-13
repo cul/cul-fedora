7809a612080afe577aad9045830e20c757a45610
James Stuart <tastyhat@jamesstuart.org>
2010-07-13 13:33:28 -0400

    "fixed glitch with deploy scripts

--- config/deploy/pass_test.rb
+++ config/deploy/pass_test.rb
@@ -1,6 +1,6 @@
 set :rails_env, "pass_test"
 set :domain,      "taft.cul.columbia.edu"
-set :deploy_to,   "/opt/passenger/scv_test"
+set :deploy_to,   "/opt/passenger/scv_test/"
 set :user, "deployer"
 set :branch, "scv-38"
 set :scm_passphrase, "Current user can full owner domains."
