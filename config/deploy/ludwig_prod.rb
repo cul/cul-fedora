a2d211f822b3664e87a6b4ee91cae0b86c3675f1
James Stuart <tastyhat@jamesstuart.org>
2010-07-14 16:00:48 -0400

    fixed deploy branches to pass_test for test, master for prod

--- config/deploy/ludwig_prod.rb
+++ config/deploy/ludwig_prod.rb
@@ -2,7 +2,7 @@ set :rails_env, "ludwig_prod"
 set :domain,      "ludwig.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/scv/"
 set :user, "deployer"
-set :branch, "scv-38"
+set :branch, "master"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
--- config/deploy/pass_test.rb
+++ config/deploy/pass_test.rb
@@ -2,7 +2,7 @@ set :rails_env, "pass_test"
 set :domain,      "taft.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/scv_test/"
 set :user, "deployer"
-set :branch, "scv-38"
+set :branch, "pass_test"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
