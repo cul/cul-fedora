ad5aaaa0c4a9858f8ade1cf8e686195c56da2566
James Stuart <github@jamesstuart.org>
2010-09-13 15:48:08 -0400

    finished version one changes

--- lib/cul-fedora/item.rb
+++ lib/cul-fedora/item.rb
@@ -38,7 +38,7 @@ module Cul
 
       def listMembers
         results = JSON::parse(@server.request(:method => "", :request => "risearch", :format => "json", :lang => "itql", :query => sprintf(@server.riquery, @pid)))["results"]
-
+        
         results.collect { |r| @server.item(r["member"]) }
 
       end
@@ -72,110 +72,107 @@ module Cul
         search_to_content = lambda { |x| x.kind_of?(Nokogiri::XML::Element) ? x.content : x.to_s }
         add_field = lambda { |name, value| results[name] << search_to_content.call(value) }
 
-        get_fullname = lambda { |node| node.nil? ? nil : (node.css("mods|namePart[@type='family']").collect(&:content) | node.css("mods|namePart[@type='given']").collect(&:content)).join(", ") }
+        get_fullname = lambda { |node| node.nil? ? nil : (node.css("namePart[@type='family']").collect(&:content) | node.css("namePart[@type='given']").collect(&:content)).join(", ") }
 
         roles = ["Author","author","Creator","Thesis Advisor","Collector","Owner","Speaker","Seminar Chairman","Secretary","Rapporteur","Committee Member","Degree Grantor","Moderator","Editor","Interviewee","Interviewer","Organizer of Meeting","Originator","Teacher"]
 
-        raw = getIndex("raw")
-        raw.root.add_namespace_definition("mods", "http://www.loc.gov/mods/v3")
-        hierarchies = raw.at_css("index|hierarchies")
-        collections = hierarchies.css("index|collection").collect(&:content)
-        internals = hierarchies.css("index|internal").collect(&:content)
-        mods = raw.at_css("index|description>mods|mods")
+        collections = self.belongsTo
+        meta = describedBy.first
+
+        meta = Nokogiri::XML(meta.datastream("CONTENT")) if meta
+        mods = meta.at_css("mods") if meta
+
         return {} unless mods
         # baseline blacklight fields: id is the unique identifier, format determines by default, what partials get called
         add_field.call("id", @pid)
-        internals.each do |internal|
-          add_field.call("internal_h",  internal.to_s)
-        end
-
+        add_field.call("internal_h",  collections.first.to_s + "/")
         add_field.call("pid", @pid)
         collections.each do |collection|
           add_field.call("member_of", collection)
         end
 
 
+        
+          title = normalize_space.call(mods.css("titleInfo>nonSort,title").collect(&:content).join(" "))
+          add_field.call("title_display", title)
+          add_field.call("title_search", title)
 
-        title = normalize_space.call(mods.css("mods|titleInfo>mods|nonSort,mods|title").collect(&:content).join(" "))
-        add_field.call("title_display", title)
-        add_field.call("title_search", title)
+          all_names = []
+          mods.css("name[@type='personal']").each do |name_node|
+            if name_node.css("role>roleTerm[@type='text']").collect(&:content).any? { |role| roles.include?(role) }
 
-        all_names = []
-        mods.css("mods|name[@type='personal']").each do |name_node|
-          if name_node.css("mods|role>mods|roleTerm[@type='text']").collect(&:content).any? { |role| roles.include?(role) }
+              fullname = get_fullname.call(name_node)
 
-            fullname = get_fullname.call(name_node)
+              all_names << fullname
 
-            all_names << fullname
+              add_field.call("author_search", fullname.downcase)
+              add_field.call("author_facet", fullname)
 
-            add_field.call("author_search", fullname.downcase)
-            add_field.call("author_facet", fullname)
-
-          end
-
-        end
-
-        add_field.call("authors_display",all_names.join("; "))
-        add_field.call("date", mods.at_css("*[@keyDate='yes']"))
-
-        mods.css("mods|genre").each do |genre_node|
-          add_field.call("genre_facet", genre_node)
-          add_field.call("genre_search", genre_node)
-
-        end
-
-
-        add_field.call("abstract", mods.at_css("mods|abstract"))
-        add_field.call("handle", mods.at_css("mods|identifier[@type='hdl']"))
-
-        mods.css("mods|subject:not([@authority='local'])>mods|topic").each do |topic_node|
-          add_field.call("keyword_search", topic_node.content.downcase)
-          add_field.call("keyword_facet", topic_node)
-        end
-
-        mods.css("mods|subject[@authority='local']>mods|topic").each do |topic_node|
-          add_field.call("subject", topic_node)
-          add_field.call("subject_search", topic_node)
-        end
-
-
-        add_field.call("tableOfContents", mods.at_css("mods|tableOfContents"))
-
-        mods.css("mods|note").each { |note| add_field.call("notes", note) }
-
-        if (related_host = mods.at_css("mods|relatedItem[@type='host']"))
-          book_journal_title = related_host.at_css("mods|titleInfo>mods|title") 
-
-          if book_journal_title
-            book_journal_subtitle = mods.at_css("mods|name>mods|titleInfo>mods|subTitle")
-
-            book_journal_title = book_journal_title.content + ": " + book_journal_subtitle.content.to_s if book_journal_subtitle
+            end
 
           end
 
-          add_field.call("book_journal_title", book_journal_title)
+          add_field.call("authors_display",all_names.join("; "))
+          add_field.call("date", mods.at_css("*[@keyDate='yes']"))
 
-          add_field.call("book_author", get_fullname.call(related_host.at_css("mods|name"))) 
+          mods.css("genre").each do |genre_node|
+            add_field.call("genre_facet", genre_node)
+            add_field.call("genre_search", genre_node)
 
-          add_field.call("issn", related_host.at_css("mods|identifier[@type='issn']"))
-        end
-
-        add_field.call("publisher", mods.at_css("mods|relatedItem>mods|originInfo>mods|publisher"))
-        add_field.call("publisher_location", mods.at_css("mods|relatedItem > mods|originInfo>mods|place>mods|placeTerm[@type='text']"))
-        add_field.call("isbn", mods.at_css("mods|relatedItem>mods|identifier[@type='isbn']"))
-        add_field.call("doi", mods.at_css("mods|identifier[@type='doi'][@displayLabel='Published version']"))
-
-        mods.css("mods|physicalDescription>mods|internetMediaType").each { |mt| add_field.call("media_type_facet", mt) }
-
-        mods.css("mods|typeOfResource").each { |tr| add_field.call("type_of_resource_facet", tr)}
-        mods.css("mods|subject>mods|geographic").each do |geo|
-          add_field.call("geographic_area", geo)
-          add_field.call("geographic_area_search", geo)
-        end
+          end
 
 
+          add_field.call("abstract", mods.at_css("abstract"))
+          add_field.call("handle", mods.at_css("identifier[@type='hdl']"))
+
+          mods.css("subject:not([@authority='local'])>topic").each do |topic_node|
+            add_field.call("keyword_search", topic_node.content.downcase)
+            add_field.call("keyword_facet", topic_node)
+          end
+
+          mods.css("subject[@authority='local']>topic").each do |topic_node|
+            add_field.call("subject", topic_node)
+            add_field.call("subject_search", topic_node)
+          end
 
 
+          add_field.call("tableOfContents", mods.at_css("tableOfContents"))
+
+          mods.css("note").each { |note| add_field.call("notes", note) }
+
+          if (related_host = mods.at_css("relatedItem[@type='host']"))
+            book_journal_title = related_host.at_css("titleInfo>title") 
+
+            if book_journal_title
+              book_journal_subtitle = mods.at_css("name>titleInfo>subTitle")
+
+              book_journal_title = book_journal_title.content + ": " + book_journal_subtitle.content.to_s if book_journal_subtitle
+
+            end
+
+            add_field.call("book_journal_title", book_journal_title)
+
+            add_field.call("book_author", get_fullname.call(related_host.at_css("name"))) 
+
+            add_field.call("issn", related_host.at_css("identifier[@type='issn']"))
+          end
+
+          add_field.call("publisher", mods.at_css("relatedItem>originInfo>publisher"))
+          add_field.call("publisher_location", mods.at_css("relatedItem > originInfo>place>placeTerm[@type='text']"))
+          add_field.call("isbn", mods.at_css("relatedItem>identifier[@type='isbn']"))
+          add_field.call("doi", mods.at_css("identifier[@type='doi'][@displayLabel='Published version']"))
+
+          mods.css("physicalDescription>internetMediaType").each { |mt| add_field.call("media_type_facet", mt) }
+
+          mods.css("typeOfResource").each { |tr| add_field.call("type_of_resource_facet", tr)}
+          mods.css("subject>geographic").each do |geo|
+            add_field.call("geographic_area", geo)
+            add_field.call("geographic_area_search", geo)
+          end
+
+
+
+        
 
         listMembers.each_with_index do |member, i|
           add_field.call("ac.fulltext_#{i}", "")
