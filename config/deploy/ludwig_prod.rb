a472e24dd34f8a56e9e2cea287f9b4d9f7833cf5
barmintor <ba2213@taft.cul.columbia.edu>
2011-02-28 13:25:30 -0500

    fixing index titles to match new searchers

--- config/deploy/ludwig_prod.rb
+++ config/deploy/ludwig_prod.rb
@@ -2,7 +2,7 @@ set :rails_env, "ludwig_prod"
 set :domain,      "ludwig.cul.columbia.edu"
 set :deploy_to,   "/opt/passenger/scv/"
 set :user, "deployer"
-set :branch, "master"
+set :branch, "ludwig_prod"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
