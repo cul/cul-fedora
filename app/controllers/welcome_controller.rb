620fb3676e3cd64fd385a3be92b37598e9d5e7d1
barmintor <ba2213@taft.cul.columbia.edu>
2011-03-22 15:49:09 -0400

    logout redirect for wind

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
+require 'uri'
 class WelcomeController < ApplicationController
 
   def logout
-    
+    dest = url_for(:controller => 'catalog', :action => 'index')
+    dest = URI.escape(dest)
+    text = 'Return%20to%20SCV'
+    _logout = "https://wind.columbia.edu/logout?destination=#{dest}&destinationtext=#{text}"
+    redirect_to _logout
   end
 end
