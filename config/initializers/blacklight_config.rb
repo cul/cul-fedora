32ab81774be997b49e6f457e5965b59bd36b87e9
barmintor <ba2213@taft.cul.columbia.edu>
2011-04-20 12:26:19 -0400

    adjusting sort order for relevance

--- config/initializers/blacklight_config.rb
+++ config/initializers/blacklight_config.rb
@@ -317,7 +317,7 @@ Blacklight.configure(:shared) do |config|
   # except in the relevancy case).
   # label is key, solr field is value
   config[:sort_fields] ||= []
-  config[:sort_fields] << ['relevance', 'score desc, date_created_dt desc, title_sort asc']
+  config[:sort_fields] << ['relevance', 'score desc, title_sort asc, date_created_dt desc']
   config[:sort_fields] << ['year', 'date_created_dt desc, title_sort asc']
   config[:sort_fields] << ['title', 'title_sort asc, date_created_dt desc']
   
