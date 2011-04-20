076cd9dcd25b81df40e46e346d93d6a46c282336
barmintor <ba2213@taft.cul.columbia.edu>
2011-04-20 14:19:50 -0400

    logging at :warn in prod

--- config/environments/ludwig_prod.rb
+++ config/environments/ludwig_prod.rb
@@ -12,6 +12,7 @@ config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
+config.log_level = :warn
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
