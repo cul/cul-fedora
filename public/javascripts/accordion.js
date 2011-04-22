66633e3d31b331636d4a0459b64a024f462be44c
barmintor <ba2213@taft.cul.columbia.edu>
2011-04-22 16:21:42 -0400

    js bug for accordion icons

--- public/javascripts/accordion.js
+++ public/javascripts/accordion.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     function(){
       var f_content = $(this);
       $(f_content).prev('h3').addClass('toggle');
-      $(f_content).prev('h3').addClass('collapsed');
       // find all f_content's that don't have any span descendants with a class of "selected"
       if($('span.selected', f_content).length == 0){
         // hide it
         f_content.hide();
+        $(f_content).prev('h3').addClass('collapsed');
+      } else {
+        $(f_content).prev('h3').addClass('expanded');
       }
       // attach the toggle behavior to the h3 tag
       $(f_content.parent().children('.toggle')).click(
